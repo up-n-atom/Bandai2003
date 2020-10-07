13495e0865ee35b1215bda78e9a82f8491a03220
Adam Jaremko <adam.jaremko@gmail.com>
2020-10-07 13:25:02 -0400

    Fix style consistency

--- BANDAI2003.v
+++ BANDAI2003.v
@@ -6,11 +6,11 @@ module BANDAI2003 (
     input SSn,
     output SO, /* Synchronous out */
     input RSTn,
-    input[7:0] ADDR, /* A-1 to A3 + A15 to A18 */
-    inout[7:0] DQ, /* Warning: Tri-state */
+    input [7:0] ADDR, /* A-1 to A3 + A15 to A18 */
+    inout [7:0] DQ, /* Warning: Tri-state */
     output ROMCEn,
     output RAMCEn,
-    output[6:0] RADDR /* ROM/RAM A15 to A21 */
+    output [6:0] RADDR /* ROM/RAM A15 to A21 */
 );
 
     reg [7:0] lckS; // Lock State - Addressed unlock sequence
@@ -56,7 +56,7 @@ module BANDAI2003 (
     wire oBR = iBR && ~OEn && WEn;
 
     assign DQ = ~LCKn && oBR ? bnkR[ADDR[1:0]] : 8'hZZ;
-    wire[7:0] iDQ = DQ;
+    wire [7:0] iDQ = DQ;
 
     integer i;
 

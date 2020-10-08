7947d2cb3920a4314728f79399c7b695b559c060
Adam Jaremko <adam.jaremko@gmail.com>
2020-10-07 21:01:01 -0400

    Remove superfluous bitmask
    Indices are bound by a 2 bit slice and cannot exceed the 4 element bank array bounds.

--- BANDAI2003.v
+++ BANDAI2003.v
@@ -65,7 +65,7 @@ module BANDAI2003 (
         input[7:0] ADDR
     );
         if (ADDR >= ADDR_LAO && ADDR <= ADDR_ROMB1)
-            fDQ = bnkR[ADDR[1:0] & 2'h3];
+            fDQ = bnkR[ADDR[1:0]];
 `ifdef BTYEMODE
         else if (ADDR == ADDR_MCTRL)
             fDQ = {7'b0, BYTEn};
@@ -92,7 +92,7 @@ module BANDAI2003 (
 `endif
         end else if (seL)
             if (ADDR >= ADDR_LAO && ADDR <= ADDR_ROMB1)
-                bnkR[ADDR[1:0] & 2'h3] = iDQ;
+                bnkR[ADDR[1:0]] = iDQ;
 `ifdef BTYEMODE
             else if (ADDR == ADDR_MCTRL)
                 BYTEn = ~iDQ;
